b969e907416e345eef06975c9d3d5761b5e5c5a6
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-09-16 01:34:28 -0400

    initial build bundle and serve

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'React';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>React Auth</h1>
+                <p>Authentication &amp; Session Cookies</p>
+            </div>
+        )
+    }
+}
+
+export default App;
